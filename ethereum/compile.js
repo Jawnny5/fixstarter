58cd8ff40ac43cadc2cfa9f506d4f562e7dd2793
“Jawnny5” <william.neal83@gmail.com>
2022-01-12 16:05:28 -0700

    campaign manager @ contributions test

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -2,15 +2,19 @@ const path = require("path");
 const solc = require("solc");
 const fs = require("fs-extra");
 
+//provide output path for build, destroy current directory @ compile time.
 const buildPath = path.resolve(__dirname, "build");
 fs.removeSync(buildPath);
 
+//provide contract path
 const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
 const source = fs.readFileSync(campaignPath, "utf-8");
 const output = solc.compile(source, 1).contracts;
 
+//build output path @ compile
 fs.ensureDirSync(buildPath);
 
+//loop over returned object, pull contract file name, concatenate .json file extension
 for (let contract in output) {
   fs.outputJSONSync(
     path.resolve(buildPath, contract.replace(":", "") + ".json"),
--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -35,4 +35,18 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
+  it("designates caller as campaign manager", async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it("allows campaign contributions and marks contributor as approver", async () => {
+    await campaign.methods.contribute().send({
+      value: "200",
+      from: accounts[1],
+    });
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
 });
